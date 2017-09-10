1ee9ee6b2d6418ec7def3ca7faf3f3e98e216ce8
E0032186\JUSTIN TAN <e0032186@u.nus.edu>
2017-09-10 14:37:23 +0800

    w5.2e

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -4,12 +4,28 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
 public class UtilsTest {
 
+    @Test
+    public void isAnyNull() {
+        //empty object
+        assertNoNull();
+
+        //No null objects
+        assertNoNull("abc", 1, 'a');
+        assertNoNull(2);
+        assertNoNull("hello");
+        assertNoNull("one", "two");
+
+        //One or more null objects
+        assertHasNull((Object) null);
+        assertHasNull((Object) null, "one");
+        assertHasNull((Object) null, 1, 2);
+        assertHasNull((Object) null, (Object) null, "one");
+    }
 
     @Test
     public void elementsAreUnique() throws Exception {
@@ -34,6 +50,14 @@ public class UtilsTest {
         assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+    }
+
+    public void assertNoNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+
+    public void assertHasNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
     }
 
     private void assertAreUnique(Object... objects) {
